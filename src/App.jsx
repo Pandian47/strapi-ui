c6dca023597da690bfc1071b2e2fa05356ba23d4
ResulticksInc <36412134+ResulticksInc@users.noreply.github.com>
2024-10-25 17:59:19 +0530

    SP: Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, NavLink, Route } from 'react-router-dom';
 
-
-// import './App.css'
-import gallery from "./assets/images/gallery01.jpg"
-import resul from "./assets/images/resul-logo.svg"
 import Home from './Pages/Home/Home';
 import Form from './Pages/Form/Form';
 
 function App() {
 
   return (
-    <>
-      <section data-bs-version="5.1" className="header1 cid-tJS9vXDdRK" id="header01-7">
-        <div className="container">
-          <Router>
-            <div>
-              <nav>
-                <a href="/">Home</a> | <a href="/form">Form</a>
-              </nav>
+    <section data-bs-version="5.1" className="header1 cid-tJS9vXDdRK" id="header01-7">
+      <div className="container">
+        <Router>
+          <div>
+            <NavLink activeClassName="active" to={`/`}><span>Home</span></NavLink>
+            <NavLink activeClassName="active" to={`/form`}><span>Form</span></NavLink>
 
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/form" element={<Form />} />
-              </Routes>
-            </div>
-          </Router>
-        </div>
-      </section>
-    </>
+            {/* <nav>
+                <a href="/">Home</a> | <a href="/form">Form</a>
+              </nav> */}
+
+            <Routes>
+              <Route path="/" element={<Home />} exact />
+              <Route path="/form" element={<Form />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+    </section>
   )
 }
 
